24aac8a26a8556df802e7306f38a586077ca197e
Jordi Bisbal <jordi.bisbal@gmail.com>
2022-10-15 20:01:00 +0200

    Updated to use maybe 4

--- src/FailureReasons/UnableToRetrieve.php
+++ src/FailureReasons/UnableToRetrieve.php
@@ -6,12 +6,26 @@ namespace j45l\AbstractDataStructures\FailureReasons;
 use j45l\maybe\Either\Reason;
 use JetBrains\PhpStorm\Pure;
 
-final class UnableToRetrieve extends Reason
+final class UnableToRetrieve implements Reason
 {
+    #[Pure] private function __construct(private readonly string $reason)
+    {
+    }
+
     #[Pure] public static function becauseTheStructureHasNotTheRequestedKey(string $key): self
     {
         return new self(
             sprintf('Unable to retrieve element because the data structure has not the requested key (%s).', $key)
         );
+    }
+
+    #[Pure] public function toString(): string
+    {
+        return (string) $this;
+    }
+
+    #[Pure]  public function __toString(): string
+    {
+        return $this->reason;
     }
 }
